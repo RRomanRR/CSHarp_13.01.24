900282198f67d19cc78d7e1b20a480bb0fe239cc
Roman <pliskarv@yandex.ru>
2024-01-15 22:29:11 +0900

    решили первую задачу

---
+++ lesson_3/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// НП, Задайте массив, которая определяет, присутствует ли заданное число в массиве. 
+// Программа должна выдать ответ: да/нет.
+
+// int[] array = { 1, 3, 8, 19, 3 };
+
+// int a = int.Parse(Console.ReadLine()!);
+
+// for (int i = 0; i < array.length; i++)
+// {
+//     if (i == array[i])
+//         Console.Write("YES");
+//     else
+//         Console.Write("NO");
+// }
+
+using System.ComponentModel;
+
+int[] array = { 1, 3, 8, 19, 3 };
+int a = int.Parse(Console.ReadLine()!);
+bool b = false;
+foreach (int element in array)
+{
+    if (element == a)
+        b = true;
+}
+
+{
+    if (b)
+        Console.WriteLine("ДА");
+    else
+        Console.WriteLine("НЕТ");
+}
