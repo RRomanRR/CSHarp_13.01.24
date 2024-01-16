4d83fc5d99488d87b4c8f1cb997e060ea2a398e9
Roman <pliskarv@yandex.ru>
2024-01-16 23:21:25 +0900

    решил пятую задачу добавил вывод

---
+++ lesson_3/lesson3_1/task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// пример программы для ввода в ручную эементов массива из 10 чисел
+
+int size = 10;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine("Введите элемент массива:");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i++;
+}
+foreach (int e in arr_int)
+{
+    Console.Write($"{e} ");
+}
+
