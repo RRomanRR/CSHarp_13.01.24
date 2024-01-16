485d5fa274175c10c162fe757a2d2f1ea745c9bb
Roman <pliskarv@yandex.ru>
2024-01-16 18:50:49 +0900

    приступили к задаче 3

--- lesson_3/lesson3_1/task1/Program.cs
+++ lesson_3/lesson3_1/task1/Program.cs
@@ -9,5 +9,5 @@ double CalculateFormula(int a, int b, int c, int d)
     return result;
 }
 
-double result = CalculateFormula(1, 2, 3, 4);
-Console.WriteLine(result)
+double result = CalculateFormula(2, 2, 3, 4);
+Console.WriteLine(result);
