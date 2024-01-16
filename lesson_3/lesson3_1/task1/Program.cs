dd6786c46bcf4f60f2cf6f2197b6bfa9a5ba5531
Roman <pliskarv@yandex.ru>
2024-01-16 16:37:12 +0900

    написали первую функцию

---
+++ lesson_3/lesson3_1/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// создадим первую функцию. Вычислить значение формулы 
+// a*b/c+d , где a, b, c, d - некоторые целые числа.
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result)
